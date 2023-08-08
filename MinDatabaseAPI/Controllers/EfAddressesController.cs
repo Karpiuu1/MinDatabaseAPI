3a8fe91517f883b63394d5df23cea2ca0bdd7e4c
Adrian <karpinski.adrian1@gmail.com>
2023-08-08 18:15:35 +0200

    V1.3 + EntitiyFramework services+controllers

---
+++ MinDatabaseAPI/Controllers/EfAddressesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinDatabaseAPI.Services;
+using MinDatabaseAPI.Models;
+using System.Collections.Generic;
+
+namespace MinDatabaseAPI.Controllers
+{
+    [ApiController]
+    [Route("api/efaddress")]
+    public class EfAddressesController : ControllerBase
+    {
+        private readonly EfCustomerService _customerService;
+
+        public EfAddressesController(EfCustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost("{customerId}")]
+        public IActionResult AddAddresses(int customerId, IEnumerable<Address> addresses)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _customerService.InsertAddresses(customerId, addresses);
+            return Ok();
+        }
+        [HttpDelete("{addressId}")]
+        public IActionResult DeleteAddress(int addressId)
+        {
+            _customerService.DeleteAddress(addressId);
+            return NoContent();
+        }
+    }
+}
---
+++ MinDatabaseAPI/Controllers/EfCustomersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinDatabaseAPI.Models;
+using MinDatabaseAPI.Services;
+using System.Collections.Generic;
+
+
+namespace MinDatabaseAPI.Controllers
+{
+    [ApiController]
+    [Route("api/efcustomer")]
+    public class EfCustomersController : ControllerBase
+    {
+        private readonly EfCustomerService _customerService;
+
+        public EfCustomersController(EfCustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCustomers()
+        {
+            var customers = _customerService.GetAllCustomers();
+            return Ok(customers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCustomerById(int id)
+        {
+            var customer = _customerService.GetCustomerById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        [HttpGet("{customerId}/addresses")]
+        public IActionResult GetAddressesByCustomerId(int customerId)
+        {
+            var addresses = _customerService.GetAddressesByCustomerId(customerId);
+            return Ok(addresses);
+        }
+
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var newCustomerId = _customerService.InsertCustomer(customer);
+            return CreatedAtAction(nameof(GetCustomerById), new { id = newCustomerId }, customer);
+        }
+    }
+}
