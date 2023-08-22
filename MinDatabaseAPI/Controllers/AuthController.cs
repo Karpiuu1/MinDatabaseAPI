b271a402981b20734b720216a9b1a609a0aaab8c
Adrian <karpinski.adrian1@gmail.com>
2023-08-22 19:37:10 +0200

    Authorization, JWT, Login

---
+++ MinDatabaseAPI/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using MinDatabaseAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using MinDatabaseAPI.Models;
+
+namespace MinDatabaseAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var token = await _authService.AuthenticateUserAsync(model.Username, model.Password);
+            if (token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(new {token });
+        }
+    }
+}
---
+++ MinDatabaseAPI/Services/AuthService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using MinDatabaseAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using MinDatabaseAPI.Models;
+
+namespace MinDatabaseAPI.Services
+{
+    public class AuthService
+    {
+        private readonly CustomerDbContext _dbContext;
+        private readonly IConfiguration _configuration;
+
+
+        public AuthService(CustomerDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _configuration = configuration;
+        }
+
+        public async Task<string> AuthenticateUserAsync(string username, string password)
+        {
+            var user = await _dbContext.Customers.FirstOrDefaultAsync(c => c.Username == username && c.Password == password);
+            if (user == null)
+            {
+                return null;
+            }
+
+            var token = GenerateJwtToken(user);
+            return token;
+        }
+
+        private string GenerateJwtToken(Customer user) 
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(12),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+    }
+}
